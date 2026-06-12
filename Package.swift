416f0d020036f080d1c7f36d5c7f16c1ff2a1e30
Alexandre Brispot <alexandre.brispot@datadome.co>
2026-06-12 14:53:45 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "CoreDataDome",
-                      url: "https://package.datadome.co/ios/CoreDataDome/0.6.0%2Bbuild.4.zip",
-                      checksum: "0fdc1b65a34732dafb96428834e9a735161b5b7f079249cde996e8188cce4e82")
+                      url: "https://package.datadome.co/ios/CoreDataDome/0.6.1%2Bbuild.1.zip",
+                      checksum: "dc836c58501ea4aa9c772ced0ffcb6163a2a787c45966d0f05722e7f654226f3")
     ]
 )
